b1b8a36793dc0ce3ff266b4c38350c580810a588
Jason Osorio Marin <waterdude50@gmail.com>
2024-01-24 08:49:42 -0500

    added some project page

--- app/components/Projects/ProjectPage.tsx
+++ app/components/Projects/ProjectPage.tsx
@@ -3,15 +3,16 @@ import Tilt from 'react-parallax-tilt';
 export default function Projects(){
     return (
             <div id='projectPage' className="h-full w-screen bg-light-background dark:bg-dark-background flex items-center justify-center">
-                <div className="grid grid-cols-4 grid-rows-2 gap-5 w-full px-7">
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex rounded-lg">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
-                    <Tilt className="h-40 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                <div className="grid grid-cols-3 grid-rows-3 gap-5 w-full px-7">
+                    <Tilt className="h-60 text-center text-black bg-light-primary dark:bg-dark-primary justify-center items-center flex rounded-lg">
+                        <div className='bg-light-accent dark:bg-dark-accent hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Testing a button look</div>
+                    </Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
+                    <Tilt className="h-60 text-center text-black bg-green-400 justify-center items-center flex">Tilt ✨</Tilt>
                 </div>
             </div>
     )
